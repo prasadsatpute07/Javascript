d8d511487a53b466f28fb5190f155e5b56f2ace2
prasadsatpute07 <satputeprasada@gmail.com>
2025-03-20 12:39:34 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Prasad");
+
+let name = "prasad" // string datatype
+let age = 20
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unqiue
+
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
